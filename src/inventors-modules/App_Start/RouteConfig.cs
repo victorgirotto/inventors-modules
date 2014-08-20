ea3200642f755d0d7af1bcdd7f591f4b2fa1e983
Victor Girotto <victor.girotto@asu.edu>
2014-08-20 14:41:41 -0700

    Update NuGet packages

--- src/inventors-modules/App_Start/RouteConfig.cs
+++ src/inventors-modules/App_Start/RouteConfig.cs
@@ -10,7 +10,9 @@ namespace WebApplication1
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.EnableFriendlyUrls();
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
         }
     }
 }
